27c3ebf4c472b8319bea312c789173dea119c80d
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2023-01-10 14:41:47 -0500

    UIChange

--- Assets/_Project/Scripts/UIManagerJ.cs
+++ Assets/_Project/Scripts/UIManagerJ.cs
@@ -9,22 +9,6 @@ public class UIManagerJ : Singleton<UIManagerJ>
     [SerializeField] private GameObject UICanvas;
     [SerializeField] private GameObject winScreen;
     [SerializeField] private GameObject loseScreen;
-    [SerializeField] private Button homeButton;
-    [SerializeField] private Button restartButton;
-    [SerializeField] private Button nextLevelButton;
-    [SerializeField] private string nextLevelName;
-
-    private void Start()
-    {
-        AddListeners();
-    }
-
-    private void AddListeners()
-    {
-        homeButton.onClick.AddListener(HomeButtonFunc);
-        restartButton.onClick.AddListener(RestartButtonFunc);
-        nextLevelButton.onClick.AddListener(NextLevelButtonFunc);
-    }
 
     public void ToggleCanvas()
     {
@@ -41,21 +25,5 @@ public class UIManagerJ : Singleton<UIManagerJ>
     {
         UICanvas.SetActive(true);
         loseScreen.SetActive(true);
-    }
-
-    //Buttons functionalities
-    private void HomeButtonFunc()
-    {
-        SceneManager.LoadScene("Home");
-    }
-
-    private void RestartButtonFunc()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    private void NextLevelButtonFunc()
-    {
-        SceneManager.LoadScene(nextLevelName);
     }
 }
