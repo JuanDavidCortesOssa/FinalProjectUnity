c24c976dbd086d583417d9225467c5d03d8b8ad5
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2023-01-16 10:01:54 -0500

    movement duration changed

--- Assets/_Project/Scripts/GameSettings.cs
+++ Assets/_Project/Scripts/GameSettings.cs
@@ -1,6 +1,6 @@
 public class GameSettings
 {
-    public static float movementAnimationDuration = 0.5f;
+    public static float movementAnimationDuration = 0.3f;
     public static float enemyHeight = 1;
     public static float playerHeight = 1;
 }
